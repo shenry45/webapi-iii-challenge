6eba50814c560655f6017fe40799efcb0c6d3dde
shenry45 <shawn45henry@gmail.com>
2019-07-30 18:48:33 -0700

    init, server deps, server routing, router middleware and methods, Get / and Get /:id complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code away!
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const postsRouter = require('./posts/postRouter.js');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.use('/posts/', postsRouter);
+
+server.get('/', (req, res) => {
+  res.send('Get / for posts API');
+})
+
+server.listen(4000, () => {
+  console.log('\n ***** Listening on Port 4000 ***** \n');
+})
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,52 @@
-const express = 'express';
+const express = require('express');
+
+const db = require('..//posts/postDb.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+// Custom Middleware
+function getReqBody(req, res, next) {
+  if (req.body) {
+    next();
+  } else {
+    res.status(500).json({
+      message: "Please submit a post ID along with this request."
+    })
+  }
+}
 
+router.get('/', async (req, res) => {
+  try {
+    const postsInfo = await db.get();
+
+    res.status(200).json({
+      success: true,
+      data: postsInfo
+    })
+  }
+  catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    })
+  }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', getReqBody, async (req, res) => {
+  try {
+    const {id} = req.params;
+    const postInfo = await db.getById(id);
+    
+    res.status(200).json({
+      success: true,
+      post: postInfo
+    })
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    })
+  }
 });
 
 router.delete('/:id', (req, res) => {
