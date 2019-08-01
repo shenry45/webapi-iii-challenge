28e44ef4f284c6dea319ffd89cdf956781afca5b
shenry45 <shawn45henry@gmail.com>
2019-08-01 06:23:25 -0700

    add var port to server.listen

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 const port = process.env.PORT || 4000;
 
-server.listen(4000, () => {
+server.listen(port, () => {
   console.log(`\n ***** Listening on Port ${port} ***** \n`);
 })
